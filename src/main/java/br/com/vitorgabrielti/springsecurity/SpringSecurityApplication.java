07180b05881d78f6aab3baccabd683c318b2891b
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-02 02:11:22 -0300

    Criando rota privada

--- src/main/java/br/com/vitorgabrielti/springsecurity/SpringSecurityApplication.java
+++ src/main/java/br/com/vitorgabrielti/springsecurity/SpringSecurityApplication.java
@@ -20,4 +20,11 @@ class HttpController {
 	String publicRoute() {
 		return "<h1> Public route, feel free to look around! </h1>";
 	}
+
+	/*criação de rota privada */
+	@GetMapping("/private")
+	String privateRoute() {
+		return "<h1> Private route, only authorized personal! </h1>";
+	}
+
 }
