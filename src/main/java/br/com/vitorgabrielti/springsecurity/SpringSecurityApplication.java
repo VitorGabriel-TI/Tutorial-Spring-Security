50e13058e0163f740873e2ead50638febcc8d2ae
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-02 01:57:09 -0300

    Initial commit

---
+++ src/main/java/br/com/vitorgabrielti/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,23 @@
+package br.com.vitorgabrielti.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
+
+@RestController
+class HttpController {
+	@GetMapping("/public")
+	String publicRoute() {
+		return "<h1> Public route, feel free to look around! </h1>";
+	}
+}
