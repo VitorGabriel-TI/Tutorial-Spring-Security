82d1d51e53f8a2098458f1b5a5b80d4a0ec1edbc
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-09 05:14:52 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/vitorgabrielti/springsecurity/SecurityConfig.java
+++ src/main/java/br/com/vitorgabrielti/springsecurity/SecurityConfig.java
@@ -1,5 +1,25 @@
 package br.com.vitorgabrielti.springsecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+            autorizeConfig -> {
+                autorizeConfig.requestMatchers("/public").permitAll();
+                autorizeConfig.requestMatchers("/logout").permitAll();
+                autorizeConfig.anyRequest().authenticated();
+            })
+        .formLogin(Customizer.withDefaults())
+        .build();
+    }
 }
